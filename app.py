6ee5881a3b16d80a107608eb53013df918538ab3
akash.deep <akash.deep@sait.ac.in>
2022-12-28 22:27:25 +0530

    send email when a lead is triggered

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, request, abort
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Webhook Sample!'
-
-@app.route('/webhook', methods=['POST'])
-def post_webhook():
-    if request.method == 'POST':
-        print("received data: ", request.json)
-        return 'success', 200
-    else:
-        print("not allowed")
-        abort(400)
+mail = Mail(app) # instantiate the mail class
+   
+# configuration of mail
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'akash.deep@sait.ac.in'
+app.config['MAIL_PASSWORD'] = 'nutanakash'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 @app.route('/webhook-get', methods=['GET', 'POST'])
@@ -20,6 +19,17 @@ def get_webhook():
     if request.method == 'GET':
         return request.args.get('hub.challenge'), 200
     else:
-        print("json is : ", request.json)
+        print("json is : ", request.json["entry"][0]["changes"][0]["value"]["leadgen_id"])
+        send_email(request.json["entry"][0]["changes"][0]["value"]["leadgen_id"])
         return request.json
     
+
+def send_email(lead_id):
+    msg = Message(
+                'Hello',
+                sender ='akash.deep@sait.ac.in',
+                recipients = ['akash.deep@essentia.dev']
+               )
+    msg.body = f'Hello Akash, You have received an email with lead id {lead_id}'
+    mail.send(msg)
+    return "Email Sent"
