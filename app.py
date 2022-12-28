8dca7503e862ee1b0722379e4515e2d792ca7b51
akash.deep <akash.deep@sait.ac.in>
2022-12-28 17:41:36 +0530

    initialize project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, abort
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Webhook Sample!'
+
+@app.route('/webhook', methods=['POST'])
+def post_webhook():
+    if request.method == 'POST':
+        print("received data: ", request.json)
+        return 'success', 200
+    else:
+        print("not allowed")
+        abort(400)
+
+
+@app.route('/webhook-get', methods=['GET'])
+def get_webhook():
+        return {"Data": "Webhook testing"}
+    
